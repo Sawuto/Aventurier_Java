3f52efb133fa9a8b22c3e567990eae082486cd13
Sawuto <enzosaccone69@gmail.com>
2025-03-02 17:29:11 -0500

    deplacement controleur

--- Aventurier/src/App/App.java
+++ Aventurier/src/App/App.java
@@ -1,7 +1,25 @@
 package App;
 
+import java.io.IOException;
+import modele.Carte;
+import modele.Hero;
+import controleur.Deplacement;
+
 public class App {
     public static void main(String[] args) {
-        
-    }	
+        try {
+            Carte carte = new Carte("/home/enzos/Téléchargements/carte.txt");
+            Hero hero = new Hero(3, 0);
+            Deplacement controleur = new Deplacement(carte, hero);
+
+            String mouvements = "SSSSEEEEEENN";
+            for (char direction : mouvements.toCharArray()) {
+                controleur.deplacer(direction);
+            }
+            
+            System.out.println("Position finale : (" + hero.getX() + ", " + hero.getY() + ")");
+        } catch (IOException e) {
+            System.out.println("Erreur : " + e.getMessage());
+        }
+    }
 }
--- Aventurier/src/controleur/Deplacement.java
+++ Aventurier/src/controleur/Deplacement.java
@@ -1,5 +1,38 @@
 package controleur;
 
+import modele.Carte;
+import modele.Hero;
+
 public class Deplacement {
-    
+    private Carte carte;
+    private Hero hero;
+
+    public Deplacement(Carte carte, Hero hero) {
+        this.carte = carte;
+        this.hero = hero;
+    }
+
+    public void deplacer(char direction) {
+        int X = hero.getX();
+        int Y = hero.getY();
+
+        switch (direction) {
+            case 'N':
+            	Y--; 
+            break;
+            case 'S': 
+            	Y++; 
+            break;
+            case 'E': 
+            	X++; 
+            break;
+            case 'O': 
+            	X--;
+            break;
+        }
+
+        if (carte.estAccessible(X, Y)) {
+            hero.setPosition(X, Y);
+        }
+    }
 }
